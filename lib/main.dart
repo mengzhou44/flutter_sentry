df71e1e1528258520c463748b8468d5418db0d29
meng zhou <mengzhou44@gmail.com>
2019-09-04 22:01:48 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// This imports the Dart Sentry.io client that sends crash reports to Sentry.io.
+import 'package:sentry/sentry.dart';
+
+import 'dsn.dart';
+
+final SentryClient sentry = new SentryClient(dsn: dsn);
+
+/// Whether the VM is running in debug mode.
+///
+/// This is useful to decide whether a report should be sent to sentry. Usually
+/// reports from dev mode are not very useful, as these happen on developers'
+/// workspaces rather than on users' devices in production.
+bool get isInDebugMode {
+  // bool inDebugMode = false;
+  // assert(inDebugMode = true);
+  // return inDebugMode;
+
+  return false;
+}
+
+Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
+  print('Caught error: $error');
+
+  if (isInDebugMode) {
+    print(stackTrace);
+    print('In dev mode. Not sending report to Sentry.io.');
+    return;
+  }
+
+  print('Reporting to Sentry.io...');
+
+  final SentryResponse response = await sentry.captureException(
+    exception: error,
+    stackTrace: stackTrace,
+  );
+
+  if (response.isSuccessful) {
+    print('Success! Event ID: ${response.eventId}');
+  } else {
+    print('Failed to report to Sentry.io: ${response.error}');
+  }
+}
+
+Future<Null> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZoned<Future<Null>>(() async {
+    runApp(new CrashyApp());
+  }, onError: (error, stackTrace) async {
+    await _reportError(error, stackTrace);
+  });
+}
+
+class CrashyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Crashy',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Crashy'),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Dart exception'),
+              elevation: 1.0,
+              onPressed: () {
+                throw new StateError('This is a Dart exception.');
+              },
+            ),
+            new RaisedButton(
+              child: new Text('async Dart exception'),
+              elevation: 1.0,
+              onPressed: () async {
+                foo() async {
+                  throw new StateError('This is an async Dart exception.');
+                }
+
+                bar() async {
+                  await foo();
+                }
+
+                await bar();
+              },
+            ),
+            new RaisedButton(
+              child: new Text('Java exception'),
+              elevation: 1.0,
+              onPressed: () async {
+                final channel = const MethodChannel('crashy-custom-channel');
+                await channel.invokeMethod('blah');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
